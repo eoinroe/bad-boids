91da69a6176e94523cfc677aa93711bca7d945bb
eoinroe <eoinroe@gmail.com>
2022-11-09 20:44:02 +0000

    optimized imports

--- record_fixture.py
+++ record_fixture.py
@@ -1,9 +1,11 @@
 from copy import deepcopy
+
 import yaml
+
 import boids
 
 before = deepcopy(boids.boids)
-boids.updateBoids(boids.boids)
+boids.update_boids(boids.boids)
 after = boids.boids
 fixture = {"before": before, "after": after}
 with open("fixture.yml", "w") as fixture_file:
